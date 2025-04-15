a72e23326c45e67692f301fea8408b9bc3c46b8e
Luca Botini <devbotini@gmail.com>
2025-04-15 00:28:59 -0300

    feat: Adicionado link de repositório do portfólio, Novo projeto de Landpage e 2 novos certificados
    Certificados: React, Nodejs, Express, SQL/NoSQL

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../../src/App.css"
+import "../../src/App.css";
 
 function Projetos() {
   const settings = {
@@ -16,13 +16,13 @@ function Projetos() {
     responsive: [
       {
         breakpoint: 1024,
-        settings: { slidesToShow: 2 }
+        settings: { slidesToShow: 2 },
       },
       {
         breakpoint: 768,
-        settings: { slidesToShow: 1 }
-      }
-    ]
+        settings: { slidesToShow: 1 },
+      },
+    ],
   };
 
   const projetos = [
@@ -30,42 +30,54 @@ function Projetos() {
       img: "login1.png",
       titulo: "Sistema Estética Automotiva",
       descricao: "Gerencia clientes por placa, nome e contato.",
-      tecnologias: "Javascript, Node.js, Express, Bootstrap",
-      link: "https://botiniwashmotos.squareweb.app" // Adicione o link aqui
+      tecnologias: "Javascript, Node.js, Express, Bootstrap 5",
+      link: "https://botiniwashmotos.squareweb.app",
     },
     {
       img: "botsDiscord.png",
       titulo: "Automação BOTS Discord",
       descricao: "Ajuda empreendedores a organizar sua loja e servidor.",
       tecnologias: "Javascript, Typescript, Node.js, Express",
-      link: "https://discord.gg/wwzFuVp9sY" // Adicione o link aqui
+      link: "https://discord.gg/wwzFuVp9sY",
     },
     {
       img: "portfolioprojeto.png",
       titulo: "Botini Portfólio",
       descricao: "Plataforma para apresentar projetos e presença digital.",
-      tecnologias: "React Vite, Javascript, Bootstrap",
-      link: "" // Adicione o link aqui
+      tecnologias: "React Vite, Javascript, Bootstrap 5",
+      link: "https://github.com/LucaBotini/portfolio-btn/",
     },
     {
       img: "basecreative.png",
       titulo: "Base Creative Network V2",
-      descricao: "Base de GTA Roleplay, para donos de servidores criarem sua cidade virtual.",
+      descricao:
+        "Base de GTA Roleplay, para donos de servidores criarem sua cidade virtual.",
       tecnologias: "Lua, Javascript, Fivem Creative Network",
-      link: "https://www.youtube.com/watch?v=ZI3xz5nCdig&t=101s" // Adicione o link aqui
+      link: "https://www.youtube.com/watch?v=ZI3xz5nCdig&t=101s",
     },
     {
       img: "ar.png",
       titulo: "Website América Rental",
-      descricao: "Website completo realizado por mim e Edvam S. para a empresa Ámerica Rental.",
+      descricao:
+        "Website completo realizado por mim e Edvam S. para a empresa Ámerica Rental.",
       tecnologias: "Wordpress, Elementor Pro",
-      link: "https://www.americarental.com.br" // Adicione o link aqui
-    }
+      link: "https://www.americarental.com.br",
+    },
+    {
+      img: "botcs.png",
+      titulo: "Landpage: Apresentação de serviço premium",
+      descricao:
+        "Landing page criada para divulgar o CS, bot com funções exclusivas.",
+      tecnologias: "React Vite, Javascript, Bootstrap 5",
+      link: "https://landpage-cs.vercel.app",
+    },
   ];
 
   return (
     <div id="projetos" className="container text-center mt-6 px-4">
-      <h1 className="text-3xl font-semibold text-gray-800 mb-8 text-white">Meus Projetos</h1>
+      <h1 className="text-3xl font-semibold text-gray-800 mb-8 text-white">
+        Meus Projetos
+      </h1>
 
       <Slider {...settings}>
         {projetos.map((projeto, index) => (
@@ -83,9 +95,15 @@ function Projetos() {
                   alt={projeto.titulo}
                 />
                 <div className="card-body p-5 h-56">
-                  <h5 className="card-title text-xl font-semibold text-gray-800">{projeto.titulo}</h5>
-                  <p className="card-text text-gray-600 mt-3">{projeto.descricao}</p>
-                  <p className="text-sm text-gray-500 mt-2"><strong>Tecnologias:</strong> {projeto.tecnologias}</p>
+                  <h5 className="card-title text-xl font-semibold text-gray-800">
+                    {projeto.titulo}
+                  </h5>
+                  <p className="card-text text-gray-600 mt-3">
+                    {projeto.descricao}
+                  </p>
+                  <p className="text-sm text-gray-500 mt-2">
+                    <strong>Tecnologias:</strong> {projeto.tecnologias}
+                  </p>
                 </div>
               </div>
             </a>
